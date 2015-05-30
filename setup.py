7d269d472f1c30494261f2647b6409d0e5e3c2ea
Victor Shyba <victor.shyba@gmail.com>
2015-05-29 23:39:27 -0300

    [feat] setup.py and initial testing structures
    Twisted 15 as dependency, since newer Soledad runs best on it.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='crypto-finance',
+      version='0.1',
+      description='Soledad encrypted synchronized\
+                  local webapp for personal finance',
+      author='Victor Shyba',
+      author_email='victor1984@riseup.net',
+      url='https://github.com/shyba/crypto-finance',
+      packages=[
+          'crypto_finance'
+      ],
+      test_suite='crypto_finance.tests',
+      install_requires=[
+          'Twisted==15.2.0'
+      ],
+      tests_require=[
+      ],
+      )
