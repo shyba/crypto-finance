8856f488c23a39fedeec02fbfe1ea30fb84a1895
Victor Shyba <victor.shyba@gmail.com>
2015-06-01 22:25:55 -0300

    [tests] configurable function to use on Auth resource
    This test expects to get a reusable function to authenticate just passing
    user and password.
    This way we can use it on AuthResource without knowledge of configuration.

--- crypto_finance/tests/test_auth.py
+++ crypto_finance/tests/test_auth.py
@@ -1,6 +1,7 @@
 from twisted.trial.unittest import TestCase
 from crypto_finance.sync import auth
 from mock import patch
+from mock import Mock
 
 
 class AuthTestCase(TestCase):
@@ -13,3 +14,13 @@ class AuthTestCase(TestCase):
         fake_srp.assert_called_once_with('api', 'ca.crt')
         instance = fake_srp.return_value
         instance.authenticate.assert_called_once_with('user', 'pass')
+
+    def test_configured_authenticator(self):
+        auth.authenticate = Mock()
+        authenticator = auth.configure_authenticator('ca.crt', 'api')
+
+        authenticator('user', 'pass')
+
+        auth.authenticate.assert_called_once_with('user', 'pass',
+                                                  cert_file='ca.crt',
+                                                  api='api')
