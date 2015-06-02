76a258b012778e4dc1d9fd8a70869ba35fc3ebf3
Victor Shyba <victor.shyba@gmail.com>
2015-06-01 22:28:30 -0300

    [feat] partial function to configure authentiction
    This will return a configured authenticator by fixing api and
    certificate path, leaving only password and user to be filled
    by caller.

--- crypto_finance/sync/auth/__init__.py
+++ crypto_finance/sync/auth/__init__.py
@@ -1,7 +1,12 @@
 from leap.auth import SRPAuth
+from functools import partial
 
 
 def authenticate(user, password, cert_file, api):
     srp_auth = SRPAuth(api, cert_file)
     auth = srp_auth.authenticate(user, password)
     return auth.__dict__.copy()
+
+
+def configure_authenticator(cert_file, api):
+    return partial(authenticate, cert_file=cert_file, api=api)
